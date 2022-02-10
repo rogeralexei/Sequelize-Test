3cacbcfb07fd1bb89b57fba1ce8b16caa5ec711f
RogerUrrutia <rogeralexei11@gmail.com>
2022-02-09 20:57:01 -0500

    New Config

--- app/db.config.js
+++ app/db.config.js
@@ -1,8 +1,8 @@
 module.exports = {
-    HOST: "ec2-34-205-46-149.compute-1.amazonaws.com",
-    USER: "iiyxcpjgtksomo",
-    PASSWORD: "7d49c451210f656fa7a7267156902764d56fd4f1551e345b8a0c144c682a0439",
-    DB: "d6bb7pipsu54at",
+    HOST: "ec2-18-215-8-186.compute-1.amazonaws.com",
+    USER: "vsatvgxjdamdjn",
+    PASSWORD: "36608dd165e908cbaa997e842e57cb0b80aefdb100256ca27253210d29d7107f",
+    DB: "d331mc1issb880",
     dialect: "postgres",
     ssl: true,
     dialectOptions: {
